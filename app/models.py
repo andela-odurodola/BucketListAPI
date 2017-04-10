abcebf3092801db22e57c05a2618d79f95a122bb
andela-odurodola <Oluwadamilola.durodola@andela.com>
2017-04-10 11:51:21 +0100

    Remove unnecessary import

--- app/models.py
+++ app/models.py
@@ -1,12 +1,10 @@
 #!flask/bin/python3/
 
+from datetime import datetime
 from flask import current_app
+from flask_login import UserMixin
 from itsdangerous import (TimedJSONWebSignatureSerializer as Serializer,
     BadSignature, SignatureExpired)
-from datetime import datetime
-
-from sqlalchemy import desc
-from flask_login import UserMixin
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from app import db
