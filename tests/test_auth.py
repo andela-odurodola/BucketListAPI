bafb7d68dbeff151a6e9fb047cd7dc1e4f16f1b2
andela-odurodola <Oluwadamilola.durodola@andela.com>
2017-04-10 11:52:14 +0100

    Add more tests for authorization

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -10,19 +10,21 @@ class TestAuthorization(BaseTest):
 
     def test_register_succesfully(self):
         endpoint = '/api/v1/auth/register'
-        credentials = {'username': 'dee', 'password': 'hello'}
+        data = {'username': 'dee', 'password': 'hello'}
 
-        response = self.client.post(endpoint, data=credentials)
+        response = self.client.post(endpoint, data=data)
         welcome_message = json.loads(response.get_data(as_text=True)).get('message')
 
         self.assertEqual("Welcome dee to the Bucketlist Service.", welcome_message)
-        self.assertTrue(response.status_code == 201)
+        self.assertEqual(response.status_code, 200)
 
     def test_register_user_persistence(self):
         endpoint = '/api/v1/auth/register'
-        credentials = {'username': 'dee', 'password': 'hello'}
+        data = {'username': 'dee', 'password': 'hello'}
+        user = User.query.filter_by(username='dee').first()
+        self.assertIsNone(user)
 
-        self.client.post(endpoint, data=credentials)
+        self.client.post(endpoint, data=data)
         user = User.query.filter_by(username='dee').first()
 
         self.assertIsNotNone(user)
@@ -32,48 +34,44 @@ class TestAuthorization(BaseTest):
         db.session.add(self.user)
         db.session.commit()
         endpoint = '/api/v1/auth/register'
-        credentials = {'username': 'damidee', 'password': 'andela'}
+        data = {'username': 'damidee', 'password': 'andela'}
 
-        response = self.client.post(endpoint, data=credentials)
+        response = self.client.post(endpoint, data=data)
         error_message = json.loads(response.get_data(as_text=True)).get('message')
 
         self.assertEqual(error_message, "The username already exist.Choose another")
-        self.assertTrue(response.status_code == 406)
+        self.assertEqual(response.status_code, 409)
 
     def test_register_user_with_blank_fields(self):
         endpoint = '/api/v1/auth/register'
-        credentials = {'username': '', 'password': ''}
+        data = {'username': '', 'password': ''}
 
-        response = self.client.post(endpoint, data=credentials)
+        response = self.client.post(endpoint, data=data)
         error_message = json.loads(response.get_data(as_text=True)).get('message')
 
         self.assertEqual("User's details cannot be empty. Please specify a username and password", error_message)
-        self.assertTrue(response.status_code == 406)
+        self.assertEqual(response.status_code, 400)
 
     def test_login_with_valid_details(self):
         db.session.add(self.user)
         db.session.commit()
         endpoint = '/api/v1/auth/login'
-        credentials = {'username': 'damidee', 'password': 'yello'}
+        data = {'username': 'damidee', 'password': 'yello'}
 
-        response = self.client.post(endpoint, data=credentials)
+        response = self.client.post(endpoint, data=data)
         login_message = json.loads(response.get_data(as_text=True)).get('message')
 
         self.assertEqual("logged in successfully as damidee", login_message)
-        self.assertTrue(response.status_code == 202)
+        self.assertEqual(response.status_code, 200)
 
     def test_invalid_login_details(self):
         db.session.add(self.user)
         db.session.commit()
         endpoint = '/api/v1/auth/login'
-        credentials = {'username': 'dadee', 'password': 'yello'}
+        data = {'username': 'dadee', 'password': 'yello'}
 
-        response = self.client.post(endpoint, data=credentials)
+        response = self.client.post(endpoint, data=data)
         error_message = json.loads(response.get_data(as_text=True)).get('message')
 
         self.assertIn("Please enter a valid username or password", error_message)
-        self.assertTrue(response.status_code == 406)
-
-
-if __name__ == '__main__':
-    unittest.main()
+        self.assertEqual(response.status_code, 400)
