7ddfabd99f97b14a42f148a93c179aaaebca056b
andela-odurodola <Oluwadamilola.durodola@andela.com>
2017-04-12 13:51:35 +0100

    Commit Body Add a decorator file Refactor the custom message file

--- app/common/decorators.py
+++ app/common/decorators.py
@@ -26,19 +26,19 @@ def invalid_id(f):
         bucketlist_id = kwargs.get('bucketlist_id')
         bucketlist = BucketList.query.filter_by(id_no=bucketlist_id).first()
         if not bucketlist:
-            abort(400, message='Bucketlist does not exist')
+            abort(404, message='Bucketlist does not exist')
         return f(*args, **kwargs)
     return decorator
 
 
 def invalid_item_id(f):
     # A decorator function that checks for a valid bucketlist item id
-    
+
     @wraps(f)
     def decorator(*args, **kwargs):
         bucketlistitem_id = kwargs.get('bucketitem_id')
         bucketlist_item = BucketListItem.query.filter_by(id_no=bucketlistitem_id).first()
         if not bucketlist_item:
-            abort(400, message='Bucketlist Item does not exist')
+            abort(404, message='Bucketlist Item does not exist')
         return f(*args, **kwargs)
     return decorator
